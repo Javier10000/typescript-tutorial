f41bef4f566ac28ce87304c8c8b646608354bd0b
Francisco Javier Verdugo <fvermen203@iescarrillo.es>
2026-09-25 14:57:58 +0200

    lo visto hasta interfaces

--- src/01-variables/demo.ts
+++ src/01-variables/demo.ts
@@ -1,3 +1,5 @@
+import { notDeepEqual } from "assert";
+import { log } from "console";
 
 let miNombre:String = "Javier";
 
@@ -17,7 +19,67 @@ if (miNombre=="Javier") {
   
   
 }
-console.log("esto se ha guardado en el mac? esto esta escrito en windows ");
+/*
+tipos de datos 
+*/
+let texto = "jose"
+let numero:number = 1.2;
+let numeroEntero:number = 1;
+let buleano:boolean = true;
+let cualquiercosa:any = "Jose"
+let nulo:null = null;
+let hola:string|null|number=null;
+let hola1:null|string;
+
+
+
+
+function saludar():void{
+  console.log("hola mundo");
+
+}
+function saludarNever():never{
+  console.log("hola mundo");
+  while (true) {
+    
+      console.log("hola");
+
+    }
+  
+}
+
+cualquiercosa = 2;
+console.log(cualquiercosa);
+
+let desconocido:unknown = "jose"
+if(typeof(desconocido)=="string"){
+    console.log(desconocido.toUpperCase());
+}
+hola1="javier";
+console.log(hola1.toUpperCase);
+/**
+ * con las comillas daleadas podemos poner un ${} y anula las comillas y se puede operar dentros
+ */
+console.log(`hola + ${desconocido+" como estas "+ hola1}`);
+console.log(true&&false);//falso
+console.log(true||false);//verdadero
+let edad:number = 3;
+console.log(--edad)
+interface Usuario{
+  nombre:string;
+  edad:number;
+  dni?:string;
+}
+let us1:Usuario = {nombre:"josee",edad:18}
+
+
+
+
+
+
+
+
+
 
 
 
